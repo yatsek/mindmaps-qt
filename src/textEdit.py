1ad60630aabae6dc207acb6d719392880e5b640a
wojtek.jurkowlaniec <wojtek.jurkowlaniec@58210eb7-3e94-6613-bd58-efd66ba3eced>
2010-11-30 17:05:46 +0000

    adding nodes functionality

--- src/textEdit.py
+++ src/textEdit.py
@@ -1,8 +1,20 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
+from graphicsItems import Node
+stack={}
 
 class textEdit(QTextEdit):
     def __init__(self,parent=None):
         super(textEdit,self).__init__(parent)
-    
+        
+        self.setCursorWidth(0) #nie chcemy kursora myszki 
+        self.setText("Lorem ipsum and so on")
+        self.connect(self, SIGNAL("selectionChanged()"),self.selectionChanged)
+        self.selectedText=""
+    def selectionChanged(self):
+        self.selectedText=self.textCursor().selectedText()
+    def mouseReleaseEvent(self,event):
+        print self.selectedText
+        if self.selectedText <> "":
+            self.parent().addItem(self.selectedText)
+            
