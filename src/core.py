4ae17b576ecf3fb5d336ed8148ae5ab2641bda83
wojtek.jurkowlaniec <wojtek.jurkowlaniec@58210eb7-3e94-6613-bd58-efd66ba3eced>
2010-12-01 17:32:44 +0000

    added functionality of creating elements from text in separate window

--- src/core.py
+++ src/core.py
@@ -4,6 +4,7 @@ from PyQt4.QtGui import *
 from random import randrange
 from graphicsItems import *
 from textEdit import *
+from createFromText import FormFromText
 class GraphicsView(QGraphicsView):
 	def __init__(self,parent=None):
 		super(GraphicsView, self).__init__(parent)
@@ -13,37 +14,43 @@ class GraphicsView(QGraphicsView):
 	def wheelEvent(self,event):
 		factor =1.41 **(-event.delta()/240.0)
 		self.scale(factor,factor)
+		
 	
 stack={}
 class Form(QDialog):
 	def __init__(self,item=None,position=None,scene=None,parent=None):
 		super(Form,self).__init__(parent)
+		
+		self.textForm=FormFromText(self)
+		self.textForm.show()
+		
 		self.view=GraphicsView()
 		self.scene =  QGraphicsScene(self)
 		self.scene.setSceneRect(0,0,100,100)
 		self.view.setScene(self.scene)
 		self.button=QPushButton("Add")
 		self.button2=QPushButton("DBG")
-		self.textedit = textEdit(self)
 		self.layout=QVBoxLayout()
 		self.layout.addWidget(self.view,0)
 		self.layout.addWidget(self.button,1)
 		self.layout.addWidget(self.button2,2)
-		self.layout.addWidget(self.textedit,0)
 		self.setLayout(self.layout)
 		self.setWindowTitle("Test")
 		self.connect(self.button, SIGNAL("clicked()"),self.addItem)
+		self.connect(self.textForm, SIGNAL("addItem"),self.addItem)
 		self.connect(self.button2, SIGNAL("clicked()"),self.deleteRandom)
 		self.count=0
-	def addItem(self,text=None,position=None):
-		#print "Width %s, height %s"%(self.view.width(),self.view.height()) 
+	def getViewRange(self):
 		x = randrange((-1)*self.view.width()/2,self.view.width()/2)
 		y = randrange((-1)*self.view.height()/2,self.view.height()/2)
 		print "Added %s %s"%(x,y)
+		return QPointF(x,y)
+	def addItem(self,text=None,position=None):
+		#print "Width %s, height %s"%(self.view.width(),self.view.height()) 	
 		if text is not None:
-			stack[self.count]=Node(QPointF(x,y),text)
+			stack[self.count]=Node(self.getViewRange(),text)
 		else:
-			stack[self.count]=Node(QPointF(width,height),QSizeF(width,height))
+			stack[self.count]=Node(self.getViewRange())
 		stack[self.count].drawOnScene(self.scene)
 		self.count+=1
 		print len(stack)
@@ -64,4 +71,6 @@ form=Form()
 rect=QApplication.desktop().availableGeometry()
 form.resize(int(rect.width() *0.8), int(rect.height() * 0.8))
 form.show()
+
+
 app.exec_()
