b769f21dd8220e938a899d01b83d8cc83d7e64f2
wojtek.jurkowlaniec <wojtek.jurkowlaniec@58210eb7-3e94-6613-bd58-efd66ba3eced>
2010-12-01 17:32:44 +0000

    added functionality of creating elements from text in separate window

---
+++ trunk/src/globalVars.py
@@ -0,0 +1,1 @@
+maxSelectionText=30
--- trunk/src/textEdit.py
+++ trunk/src/textEdit.py
@@ -1,20 +1,39 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from graphicsItems import Node
-stack={}
-
+import globalVars
+str="""Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
+Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
+Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
+Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
+Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
+"""
 class textEdit(QTextEdit):
     def __init__(self,parent=None):
         super(textEdit,self).__init__(parent)
-        
         self.setCursorWidth(0) #nie chcemy kursora myszki 
-        self.setText("Lorem ipsum and so on")
-        self.connect(self, SIGNAL("selectionChanged()"),self.selectionChanged)
-        self.selectedText=""
-    def selectionChanged(self):
-        self.selectedText=self.textCursor().selectedText()
+        self.setText(str)
+        
+    def checkLength(self,text):
+        if len(text) >globalVars.maxSelectionText:
+            msgbox=QMessageBox()
+            msgbox.setText("Text is too long")
+            msgbox.setStandardButtons(QMessageBox.Ok)
+            msgbox.exec_()
+            return False
+        if len(text) <3:
+            return False
+        return True
     def mouseReleaseEvent(self,event):
-        print self.selectedText
-        if self.selectedText <> "":
-            self.parent().addItem(self.selectedText)
-            
+        """Handles selection"""
+        selectedText=self.textCursor().selectedText()
+        if not self.checkLength(selectedText):
+            return
+        self.emit(SIGNAL("addItemToList"),selectedText)
+        print selectedText
+        format=QTextCharFormat()
+        format.setFontWeight(QFont.Bold)
+        brush=QBrush()
+        brush.setColor(Qt.green)
+        format.setBackground(brush)
+        self.textCursor().mergeCharFormat(format)
