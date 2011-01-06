201d805d2fd7782f6bbe75848c31dd8f8823f267
wojtek.jurkowlaniec <wojtek.jurkowlaniec@58210eb7-3e94-6613-bd58-efd66ba3eced>
2011-01-06 06:56:40 +0000

    zmaina spacji na tabulacje + proby  focus na QTextInput

--- trunk/src/graphicsItems.py
+++ trunk/src/graphicsItems.py
@@ -3,94 +3,106 @@ from PyQt4.QtGui import *
 import globalVars as globalV
 from textEdit import *
 class TextItem(QGraphicsTextItem):
-    def __init__(self,text,position,font=globalV.fontNode,matrix=QMatrix()):
-        super(TextItem,self).__init__(text)
-        self.setFlags(QGraphicsItem.ItemIsSelectable|QGraphicsItem.ItemIsMovable)
-        self.setFont(font)
-        self.setPos(position)
-        #self.setMatrix(matrix)
-        self.ItemSendsGeometryChanges=True
+	def __init__(self,text,position,font=globalV.fontNode,matrix=QMatrix()):
+		super(TextItem,self).__init__(text)
+		self.setFlags(QGraphicsItem.ItemIsSelectable|QGraphicsItem.ItemIsMovable)
+		self.setFont(font)
+		self.setPos(position)
+		self.ItemSendsGeometryChanges=True
 class EllipsisItem(QGraphicsEllipseItem):
-    def __init__(self,position,size=globalV.nodeSize,color=Qt.blue,penStyle=Qt.PenStyle(2),penWidth=2):
-        super(EllipsisItem,self).__init__()
-        self.setFlags(QGraphicsItem.ItemIsSelectable|QGraphicsItem.ItemIsMovable)
-        #self.prepareGeometryChange()
-        self.setRect(QRectF(position,size))
-        self.setMatrix(QMatrix())
-        self.ItemSendsGeometryChanges=True
-        def setPenStyle(self):
-            self.pen=QPen()
-            self.pen.setColor(color)
-            self.pen.setStyle(penStyle)
-            self.pen.setWidth(penWidth)
-            self.setPen(self.pen)
-        setPenStyle(self)
-        
+	def __init__(self,position,size=globalV.nodeSize,color=Qt.blue,penStyle=Qt.PenStyle(2),penWidth=2):
+		super(EllipsisItem,self).__init__()
+		self.setFlags(QGraphicsItem.ItemIsSelectable|QGraphicsItem.ItemIsMovable)
+		self.setRect(QRectF(position,size))
+		self.setMatrix(QMatrix())
+		self.ItemSendsGeometryChanges=True
+		def setPenStyle(self):
+			self.pen=QPen()
+			self.pen.setColor(color)
+			self.pen.setStyle(penStyle)
+			self.pen.setWidth(penWidth)
+			self.setPen(self.pen)
+		setPenStyle(self)
+		
 class Node(QGraphicsItemGroup):
-    """Documentation"""
-    def __init__(self,position,text="Override",parent=None):
-        super(Node,self).__init__()
-        self.setPos(position)
-        self.children =[] #children of Node
-        self.parent=parent #parent of the Node  
-        self.setFlags(QGraphicsItem.ItemIsSelectable|QGraphicsItem.ItemIsMovable)
-        self.ellipsis=EllipsisItem(position)
-        self.ellipsis.setParentItem(self)
-        self.text=TextItem(text,self.ellipsisCenter())
-        self.text.setParentItem(self)
-        self.setZValue(-1) #being on top
-        
-        self.addToGroup(self.ellipsis)
-        self.addToGroup(self.text)
-        self.setScale(1.41)
-    def shape(self):
-        #define shape of item
-        path=QPainterPath()
-        path.addEllipse(self.ellipsis.rect())
-        return path
-        
-    def runEditingText(self):
-        print "DoubleClickOnText"
-        print self.scene()
-        local_text_pos=self.text.pos()
-        #self.addToGroup(self.textEdit)
-        
-        #translacja na scene, dodanie widgetu etc
-        #self.scene().addWidget
-        print "RunEditingText "
-        return local_text_pos,self.text.toPlainText()
-        #self.textEdit.show()
-    def drawOnScene(self,scene):
-        scene.addItem(self)
-    def ellipsisCenter(self):
-        return self.ellipsis.rect().center()
-    #very important function - handles item change and so on
-    def itemChange(self,change,variant):
-        if change == QGraphicsItemGroup.ItemFlagsChange:
-            print "Item position change"
-        else:
-            #print self.pos()
-            pass
-            #print change
-        return QGraphicsItemGroup.itemChange(self,change, variant)
-    
+	"""Documentation"""
+	def __init__(self,position,text="Override",parent=None):
+		super(Node,self).__init__()
+		self.setPos(position)
+		self.children =[] #children of Node
+		self.parent=parent #parent of the Node	
+		self.setFlags(QGraphicsItem.ItemIsSelectable|QGraphicsItem.ItemIsMovable)
+		self.ellipsis=EllipsisItem(position)
+		self.ellipsis.setParentItem(self)
+		self.text=TextItem(text,self.ellipsisCenter())
+		self.text.setParentItem(self)
+		self.setZValue(-1) #being on top
+		
+		self.addToGroup(self.ellipsis)
+		self.addToGroup(self.text)
+		self.setScale(1.41)
+	def shape(self):
+		#define shape of item
+		path=QPainterPath()
+		path.addEllipse(self.ellipsis.rect())
+		return path
+		
+	def runEditingText(self):
+		print "DoubleClickOnText"
+		print self.scene()
+		local_text_pos=self.text.pos()
+		#self.addToGroup(self.textEdit)
+		
+		#translacja na scene, dodanie widgetu etc
+		#self.scene().addWidget
+		print "RunEditingText "
+		return local_text_pos,self.text.toPlainText()
+		#self.textEdit.show()
+	def drawOnScene(self,scene):
+		scene.addItem(self)
+	def ellipsisCenter(self):
+		return self.ellipsis.rect().center()
+	#very important function - handles item change and so on
+	def itemChange(self,change,variant):
+		if change == QGraphicsItemGroup.ItemFlagsChange:
+			print "Item position change"
+		else:
+			#print self.pos()
+			pass
+			#print change
+		return QGraphicsItemGroup.itemChange(self,change, variant)
+	
 class inputOnView(QWidget):
-    def __init__(self,text="Overload",rect=None,_parent=None):
-        super(inputOnView,self).__init__(_parent)
-        self.textedit=QTextEdit(text)
-        self.layout=QVBoxLayout()
-        self.layout.addWidget(self.textedit)
-        self.setLayout(self.layout)
-    def focusInEvent(self,event):
-        print "Focus in event"
-        return QWidget.focusInEvent(self,event)
-    def focusOutEvent(self,event):
-        print "Focus in event"
-        return QWidget.focusOutEvent(self,event)
-    def keyPressEvent(self,event):
-        if event.key() == Qt.Key_Enter:
-            print "Enter pressed"
-            self.textedit.selectAll()
-            self.emit(SIGNAL("editFinish"),self.textedit.textCursor().selectedText())
-        else:
-            return QWidget.keyPressEvent(self,event)
+	def __init__(self,text="Overload",rect=None,_parent=None):
+		super(inputOnView,self).__init__(_parent)
+		self.textedit=textEdit(text)
+		self.layout=QVBoxLayout()
+		self.layout.addWidget(self.textedit)
+		self.setLayout(self.layout)
+		self.connect(self.textedit, SIGNAL("endEdit"),self.applyText)
+	def focusOutEvent(self,event):
+		print "Focus in event"
+		return QWidget.focusOutEvent(self,event)
+	def focusInEvent(self,event):
+		print "Focus in event"
+		self.textedit.grabKeyboard()
+		return QWidget.focusInEvent(self,event)
+	def applyText(self,text):
+		self.textEdit.ungrabKeyboard()
+		self.emit(SIGNAL("editFinish"),text)
+	class textEdit(QTextEdit):
+		def __init__(self,text,parent=None):
+			super(textEdit,self).__init__(parent)
+			self.setFocusPolicy(Qt.ClickFocus)
+		def keyPressEvent(self,event):
+			print event.key()
+			if event.key() == Qt.Key_Enter:
+				self.selectAll()
+				text=self.textCursor(),selectedText()
+				self.emit(SIGNAL('endEdit'),text)
+			else:
+				return QWidget.keyPressEvent(self,event)
+		def focusOutEvent(self,event):
+			print "Focus in event"
+			return QWidget.focusOutEvent(self,event)
+
--- trunk/src/textEdit.py
+++ trunk/src/textEdit.py
@@ -8,32 +8,32 @@ Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
 Lorem ipsum and so onLorem ipsum and so onLorem ipsum and so on
 """
 class textEdit(QTextEdit):
-    def __init__(self,parent=None):
-        super(textEdit,self).__init__(parent)
-        self.setCursorWidth(0) #nie chcemy kursora myszki 
-        self.setText(str)
-        self.setFont(globalV.fontFromText)
-        
-    def checkLength(self,text):
-        if len(text) >globalV.maxSelectionText:
-            msgbox=QMessageBox()
-            msgbox.setText("Text is too long")
-            msgbox.setStandardButtons(QMessageBox.Ok)
-            msgbox.exec_()
-            return False
-        if len(text) <globalV.minSelectionText:
-            return False
-        return True
-    def mouseReleaseEvent(self,event):
-        """Handles selection"""
-        selectedText=self.textCursor().selectedText()
-        if not self.checkLength(selectedText):
-            return
-        self.emit(SIGNAL("addItemToList"),selectedText)
-        print selectedText
-        format=QTextCharFormat()
-        format.setFontWeight(QFont.Bold)
-        brush=QBrush()
-        brush.setColor(Qt.green)
-        format.setBackground(brush)
-        self.textCursor().mergeCharFormat(format)
+	def __init__(self,parent=None):
+		super(textEdit,self).__init__(parent)
+		self.setCursorWidth(0) #nie chcemy kursora myszki 
+		self.setText(str)
+		self.setFont(globalV.fontFromText)
+		
+	def checkLength(self,text):
+		if len(text) >globalV.maxSelectionText:
+			msgbox=QMessageBox()
+			msgbox.setText("Text is too long")
+			msgbox.setStandardButtons(QMessageBox.Ok)
+			msgbox.exec_()
+			return False
+		if len(text) <globalV.minSelectionText:
+			return False
+		return True
+	def mouseReleaseEvent(self,event):
+		"""Handles selection"""
+		selectedText=self.textCursor().selectedText()
+		if not self.checkLength(selectedText):
+			return
+		self.emit(SIGNAL("addItemToList"),selectedText)
+		print selectedText
+		format=QTextCharFormat()
+		format.setFontWeight(QFont.Bold)
+		brush=QBrush()
+		brush.setColor(Qt.green)
+		format.setBackground(brush)
+		self.textCursor().mergeCharFormat(format)
