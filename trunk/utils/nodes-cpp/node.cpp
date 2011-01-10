2ade00c9c5a346b193930d5340c4a479f9f7bd72
wojtek.jurkowlaniec <wojtek.jurkowlaniec@58210eb7-3e94-6613-bd58-efd66ba3eced>
2011-01-10 22:00:41 +0000

    changes in example - not movable nodes

--- trunk/utils/nodes-cpp/node.cpp
+++ trunk/utils/nodes-cpp/node.cpp
@@ -1,52 +1,13 @@
- /****************************************************************************
- **
- ** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
- ** All rights reserved.
- ** Contact: Nokia Corporation (qt-info@nokia.com)
- **
- ** This file is part of the examples of the Qt Toolkit.
- **
- ** $QT_BEGIN_LICENSE:LGPL$
- ** Commercial Usage
- ** Licensees holding valid Qt Commercial licenses may use this file in
- ** accordance with the Qt Commercial License Agreement provided with the
- ** Software or, alternatively, in accordance with the terms contained in
- ** a written agreement between you and Nokia.
- **
- ** GNU Lesser General Public License Usage
- ** Alternatively, this file may be used under the terms of the GNU Lesser
- ** General Public License version 2.1 as published by the Free Software
- ** Foundation and appearing in the file LICENSE.LGPL included in the
- ** packaging of this file.  Please review the following information to
- ** ensure the GNU Lesser General Public License version 2.1 requirements
- ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
- **
- ** In addition, as a special exception, Nokia gives you certain additional
- ** rights.  These rights are described in the Nokia Qt LGPL Exception
- ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
- **
- ** GNU General Public License Usage
- ** Alternatively, this file may be used under the terms of the GNU
- ** General Public License version 3.0 as published by the Free Software
- ** Foundation and appearing in the file LICENSE.GPL included in the
- ** packaging of this file.  Please review the following information to
- ** ensure the GNU General Public License version 3.0 requirements will be
- ** met: http://www.gnu.org/copyleft/gpl.html.
- **
- ** If you have questions regarding the use of this file, please contact
- ** Nokia at qt-info@nokia.com.
- ** $QT_END_LICENSE$
- **
- ****************************************************************************/
-
  #include <QGraphicsScene>
  #include <QGraphicsSceneMouseEvent>
  #include <QPainter>
  #include <QStyleOption>
-
+ #include <QMouseEvent>
+ #include <stdio.h>
  #include "edge.h"
  #include "node.h"
  #include "graphwidget.h"
+#include <string.h>
 
  Node::Node(GraphWidget *graphWidget)
      : graph(graphWidget)
@@ -55,8 +16,27 @@
      setFlag(ItemSendsGeometryChanges);
      setCacheMode(DeviceCoordinateCache);
      setZValue(-1);
+	movable=true;
  }
-
+ Node::Node(GraphWidget * graphWidget, bool _movable)
+	:graph(graphWidget)
+ {
+     setFlag(ItemIsMovable);
+     setFlag(ItemSendsGeometryChanges);	 
+	setCacheMode(DeviceCoordinateCache);
+	setZValue(-1);
+	movable=_movable;
+	
+ }
+ void Node::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
+ {
+	 movable=!movable;
+	 char tmp[4];
+	 if (movable) strcpy(tmp,"yes");
+	 else strcpy(tmp,"no");
+	 printf("doubleClick!, node is now %s \n",tmp);
+	 QGraphicsItem::mouseDoubleClickEvent(event);
+ }
  void Node::addEdge(Edge *edge)
  {
      edgeList << edge;
@@ -118,7 +98,7 @@
  {
      if (newPos == pos())
          return false;
-
+	if (!movable)return false;
      setPos(newPos);
      return true;
  }
